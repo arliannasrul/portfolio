fb47be2d89826f1705d874d1f861ce51f7b4560e
arlianansrul <arliannasrul@gmail.com>
2025-05-20 17:47:22 +0700

    footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
                   onClick={() => setIsOpen(!isOpen)}
                   className=" top-0 px-4 py-2 block fixed right-4 lg:hidden 
                     hover:scale-105 transition-all duration-300 ease-in-out
-                     rounded-lg hover:bg-gray-100/50
+                     rounded-lg 
                     z-60 "
                 >
                   <div
